da104a6420be88cba5e0bc78b4b807db90a7351d
Miguel Angel Villamil <angelvillamil1234@gmail.com>
2022-08-02 00:56:21 -0300

    Homework finalizada

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,17 +1,25 @@
-'use strict'
+"use strict";
 
 function BinarioADecimal(num) {
-  // tu codigo aca
+  
+  let binario = num.split("").reverse().join("");
+  let decimal = 0;
 
+  for(let c = 0 ; c < binario.length ; c++){
+    decimal += parseInt(binario[c] * (2**c));
+  }
+
+  return decimal;
 }
 
 function DecimalABinario(num) {
-  // tu codigo aca
-
+  
+  let binario = num.toString(2);
+  
+  return binario;
 }
-
 
 module.exports = {
-  BinarioADecimal,
-  DecimalABinario,
-}
+    BinarioADecimal,
+    DecimalABinario,
+};

a3683de5b1274fd473cd46eee9e0becec1dab69f
Miguel Angel Villamil <angelvillamil1234@gmail.com>
2022-08-24 14:27:42 -0300

    Homework en proceso

--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -15,6 +15,42 @@ function mergeSort(array) {
   // Devolver el array ordenado resultante
   // Tu código:
 
+  if(array.length < 2){
+    return array; 
+  }
+
+  let middle = parseInt(array.length / 2);
+
+  let left = array.slice(0, middle);
+
+  let right = array.slice(middle);
+
+  mergeSort(left);
+  mergeSort(right);
+
+  let merge = (left, right) => {
+
+    let result = [];
+
+    while (left.length && right.length) {
+
+      if(left[0] < right[0]){
+
+        result.push(left[0]);
+        left.shift();
+
+      } else {
+
+        result.push(right[0]);
+        right.shift();
+
+      }
+    }
+
+    return result.concat(left, right);
+  } 
+
+  return merge(mergeSort(left), mergeSort(right));
 }
 
 // No modificar nada debajo de esta línea
